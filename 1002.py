3081f47d5fa8b63aff7115f7234e077b964388af
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2023-03-04 20:06:22 -0300

    fix: Subindo questões do beecrowd

---
+++ 1002.py
@@ -0,0 +1,5 @@
+aio = float(raw_input())
+pi = 3.14159
+area = raio**2 * pi
+
+print(f{area:.4f})
---
+++ 1014.py
@@ -0,0 +1,6 @@
+km = int(input())
+litros = float(input())
+
+km_l = km  / litros
+
+print("{:.3f} km/l".format(km_l))
